a9b824a80369097da7197b296deddbe74db6afb1
Dusch4593 <brandondusch@gmail.com>
2021-11-10 16:20:16 -0500

    refactor: added urls and assetts to make PWA offline-capable.

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,19 @@
+const assetsName = "PWA_ASSETS"
+const urls = [
+  "/",
+  "/js/app.js",
+  "/js/handlers.js",
+  "/data/activities.json"
+]
+
+// Install assets
+self.addEventListener('install', event => {
+  caches.open(assetsName)
+    .then(cache => {
+      cache.addAll(urls)
+    })
+})
+
 self.addEventListener('fetch', event => {
-  
+
 })
